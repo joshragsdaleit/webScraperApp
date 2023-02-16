8e6417723c06c8c45e7e07b2ef39c4c2c4e0fae3
josh.ragsdale.it@gmail.com <josh.ragsdale.it@gmail.com>
2023-02-16 00:19:00 -0600

    Update to add Wikipedia button, clean up verbiage

--- OSA.py
+++ OSA.py
@@ -6,7 +6,7 @@ import requests
 import webbrowser
 
 # Tag options to select
-switch_options = ['a', 'title', 'p', 'Full HTML', 'Get All Text', 'span']
+switch_options = ['a', 'title', 'p', 'Full HTML', 'Get All Text', 'span', 'id']
 
 customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -20,7 +20,7 @@ class App(customtkinter.CTk):
         super().__init__()
 
         # configure window
-        self.title("scrapeApp1.0.py")
+        self.title("OSA_V1.0.py")
         self.geometry(f"{1150}x{500}")
 
         # configure grid layout (4x4)
@@ -38,11 +38,11 @@ class App(customtkinter.CTk):
         self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
         self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, text="Google Web Search", command=self.sidebar_button_event2)
         self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
-        #  self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
-        #  self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
+        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, text="Wikipedia Search", command=self.sidebar_button_event3)
+        self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
         self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
-        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Dark", "Light", "System"],
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Dark (Default)", "Light", "System"],
                                                                        command=self.change_appearance_mode_event)
         self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
         self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
@@ -62,11 +62,11 @@ class App(customtkinter.CTk):
         self.textbox.grid(row=0, rowspan=2, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
 
         # create scrollable frame
-        self.scrollable_frame = customtkinter.CTkScrollableFrame(self, label_text="Scrape Options")
+        self.scrollable_frame = customtkinter.CTkScrollableFrame(self, label_text="Scraping Options")
         self.scrollable_frame.grid(row=0, rowspan=1, column=3, padx=(20, 0), pady=(20, 0), sticky="nsew")
         self.scrollable_frame.grid_columnconfigure(0, weight=1)
         self.scrollable_frame_switches = [] # Initializing array for Switch objects
-        for i in range(len(switch_options)): # Creating Switches from the switch options
+        for i in range(len(switch_options)): # Creating Switches from the switch options, instating them into the grid
             switch = customtkinter.CTkSwitch(master=self.scrollable_frame, text=switch_options[i])
             switch.grid(row=i, column=0, padx=10, pady=(0, 20))
             self.scrollable_frame_switches.append(switch)
@@ -75,44 +75,53 @@ class App(customtkinter.CTk):
                 self.scrollable_frame_switches[i].toggle()             
 
         # Donate to me please? Broke dad with four kids and relevant experience - I could really use a dev job too!
-        self.donate = customtkinter.CTkButton(master=self, text="Donate to the Creator!", command=self.donate_button_press, border_width=2)
+        self.donate = customtkinter.CTkButton(master=self, text="Donate to the Creator", command=self.donate_button_press, border_width=2)
         self.donate.grid(row=1, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
         # set default value for Textbox
         self.textbox.insert(0.0, '''Welcome to my rudimentary web scraper!
 You can use the output of these queries to build web scraping tools, or
-to query specific websites for an actionable DOM tree.\n
+to query specific websites for an actionable DOM tree.
+
 Use the 'Google Web Search to the left to pre-fill a search URL for Google.
 In the future, I plan to add the capability to build a custom input for querying
 the DOM, to make it a one-size-fits-all tool. 
 ''')
         
 
-    def change_appearance_mode_event(self, new_appearance_mode: str):
+    def change_appearance_mode_event(self, new_appearance_mode: str): # Function to update Appearance defaults that TKInter allows
         customtkinter.set_appearance_mode(new_appearance_mode)
 
-    def change_scaling_event(self, new_scaling: str):
+    def change_scaling_event(self, new_scaling: str): # Function to update Scaling
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
         customtkinter.set_widget_scaling(new_scaling_float)
 
-    def sidebar_button_event(self):
+    def sidebar_button_event(self): # Clicking the 'about overland' button will take you to my website
         webbrowser.open('http://www.overlandtechnicalsolutions.com', new=2)
 
-    def sidebar_button_event2(self):
+    def sidebar_button_event2(self): # fill Entry box with Google Search default URL 
         self.entry.delete(0, tkinter.END)
         self.entry.insert(0, "https://www.google.com/search?q=")
 
-    def donate_button_press(self):
+    def sidebar_button_event3(self): # fill Entry box with Google Search default URL 
+        self.entry.delete(0, tkinter.END)
+        self.entry.insert(0, "https://en.wikipedia.org/wiki/")        
+
+    def donate_button_press(self): # Open CashApp for donations
         webbrowser.open('https://cash.app/$Jrags2010', new=2)
 
     def scrape_button_press(self):
         # Clear the textbox
         self.textbox.delete("0.0", tkinter.END)
-        #  Performing Request for data
+        #  Perform Request for data based on input from self.entry
         url = self.entry.get()
-        response = requests.get(url, timeout=5)
+        try:
+            response = requests.get(url, timeout=5) 
+        except Exception as err:
+            self.textbox.insert(tkinter.END, "" + str(err) + " - Did you supply a URL?\n")
+
         content = BeautifulSoup(response.content, "html.parser") #  Gathering Page Content
-        i = 0
+        i = 0 # Initializing variable for for loop
         for item in range(len(switch_options)): # for all items inside switch_options
             self.textbox.insert(tkinter.END, "\nSearching response for " + switch_options[i] + " tags. \n" + switch_options[i] + " found: \n")
             if self.scrollable_frame_switches[i].get() == True: # if the switch is enabled
@@ -121,61 +130,30 @@ the DOM, to make it a one-size-fits-all tool.
                         for result in content.find_all('a'):  # result is now the a tags
                             final_result = result.get('href') # and final_result is now the href value
                             # if (".com" or ".net" or ".org") in final_result: 
-                            self.textbox.insert(tkinter.END, "" + str(final_result) + "\n")
-                    elif switch_options[i] == 'Full HTML': 
-                        full_html = content.prettify()
-                        self.textbox.insert(tkinter.END, full_html)
-                    elif switch_options[i] == 'Get All Text':
-                        all_text = content.get_text()
-                        self.textbox.insert(tkinter.END, all_text)
-                    else:
-                        results = content.find_all(switch_options[i])
+                            self.textbox.insert(tkinter.END, "" + str(final_result) + "\n") # fill final_result to the textbox
+                    elif switch_options[i] == 'Full HTML':  # If user wants the Full HTML structure
+                        full_html = content.prettify() # Make it pretty
+                        self.textbox.insert(tkinter.END, full_html) # Fill the textbox
+                    elif switch_options[i] == 'Get All Text': # If they want all the text from the page, a la get_text()
+                        all_text = content.get_text() # Then do so
+                        self.textbox.insert(tkinter.END, all_text) # Once again, fill the textbox
+                    else: # For switch options where the text matches the query, via bs4 documentation 
+                        results = content.find_all(switch_options[i]) # Perform the query based on the switch text
                         for result in results:
-                            final_result = result.get_text()
-                            self.textbox.insert(tkinter.END, final_result + "\n") 
-                    i += 1
+                            final_result = result.get_text() # Pull the text from the results
+                            self.textbox.insert(tkinter.END, final_result + "\n")  # Fill textbox with return data
+                    i += 1 # Increment the variable
                 except Exception as err:
                             self.textbox.insert(tkinter.END, "An exception occurred with a '" + switch_options[i] + "' tag. " + str(err) + "\n")
-            else:
+            else: # If a Switch is disabled
                 self.textbox.insert(tkinter.END, "We didn't look for " + switch_options[i] + ' as the option is disabled.\n')
                 i += 1
         
-        '''
-        for item in self.scrollable_frame_switches:
-            switches_text = self.scrollable_frame_switches[i]
-            if switches_text.get() == True:
-                result = content.find_all(switches_text)
-                
-            i += 1
-        '''
-'''
-
-        self.scrollable_frame_switches = []
-        for i in range(len(switch_options)):
-            switch = customtkinter.CTkSwitch(master=self.scrollable_frame, text=f"{switch_options[i]}")
-
-As the error message indicates, the tk module does not have a function named get. 
-It might have plenty of classes whose instances have a get method, but you can't access them the way you're doing.
-
-If you're trying to get the contents of the Entry, you should assign it to a name, and call get on that instead:
-
-def EP(): # Enter inputs from values typed in
-      xf_In = e_xf.get(e_xf)
-      print(xf_In)
-Note that this assignment is different from doing e_xf = tk.Entry(root).grid(row=0, column=1). 
-If you do that, then e_xf will be bound to the return value of grid, rather than the Entry instance. 
-Grid returns None, so trying to call get on that would only give you an AttributeError.
-
-#...
-
-e_xf = tk.Entry(root)
-e_xf.grid(row=0, column=1)
-
-https://www.crummy.com/software/BeautifulSoup/bs4/doc/
-
-'''
 
 
+# BS4 Documentation for reference - https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+
+# Call Main loop
 if __name__ == "__main__":
     app = App()
     app.mainloop()
