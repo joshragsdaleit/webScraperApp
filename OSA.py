e1d72f268d66de290bc68573d3c11c6f6fe8678f
josh.ragsdale.it@gmail.com <josh.ragsdale.it@gmail.com>
2023-02-16 00:41:37 -0600

    placeholder. update the .bat file

--- OSA.py
+++ OSA.py
@@ -123,21 +123,25 @@ the DOM, to make it a one-size-fits-all tool.
         content = BeautifulSoup(response.content, "html.parser") #  Gathering Page Content
         i = 0 # Initializing variable for for loop
         for item in range(len(switch_options)): # for all items inside switch_options
-            self.textbox.insert(tkinter.END, "\nSearching response for " + switch_options[i] + " tags. \n" + switch_options[i] + " found: \n")
             if self.scrollable_frame_switches[i].get() == True: # if the switch is enabled
+                self.textbox.insert(tkinter.END, "Analyzing results for " + switch_options[i] + ": \n")
                 try:
                     if switch_options[i] == 'a':    # and the tag to search is 'a'
+                        self.textbox.insert(tkinter.END, "Results for 'a href':\n")
                         for result in content.find_all('a'):  # result is now the a tags
                             final_result = result.get('href') # and final_result is now the href value
                             # if (".com" or ".net" or ".org") in final_result: 
                             self.textbox.insert(tkinter.END, "" + str(final_result) + "\n") # fill final_result to the textbox
                     elif switch_options[i] == 'Full HTML':  # If user wants the Full HTML structure
+                        self.textbox.insert(tkinter.END, "Full HTML results:\n")
                         full_html = content.prettify() # Make it pretty
                         self.textbox.insert(tkinter.END, full_html) # Fill the textbox
                     elif switch_options[i] == 'Get All Text': # If they want all the text from the page, a la get_text()
+                        self.textbox.insert(tkinter.END, "All Text from DOM:\n")
                         all_text = content.get_text() # Then do so
                         self.textbox.insert(tkinter.END, all_text) # Once again, fill the textbox
                     else: # For switch options where the text matches the query, via bs4 documentation 
+                        self.textbox.insert(tkinter.END, "Results for '" + switch_options[i] + "':\n")
                         results = content.find_all(switch_options[i]) # Perform the query based on the switch text
                         for result in results:
                             final_result = result.get_text() # Pull the text from the results
@@ -146,7 +150,6 @@ the DOM, to make it a one-size-fits-all tool.
                 except Exception as err:
                             self.textbox.insert(tkinter.END, "An exception occurred with a '" + switch_options[i] + "' tag. " + str(err) + "\n")
             else: # If a Switch is disabled
-                self.textbox.insert(tkinter.END, "We didn't look for " + switch_options[i] + ' as the option is disabled.\n')
                 i += 1
         
 
